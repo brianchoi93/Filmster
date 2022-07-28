569400aadf713205afa673ae49b545a1ddca2682
brianchoi93 <brianchoi93@gmail.com>
2022-07-28 09:43:54 -0700

    Setup intheaters component

--- src/components/inTheaters/InTheaters.js
+++ src/components/inTheaters/InTheaters.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function InTheaters(props) {
+function InTheaters() {
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('http://localhost:8000/api/movies')
+      .then((res) => res.json())
+      .then((json)=>{
+        setMovies(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("error")
+        setError('Something went wrong! Please try again.');
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Data is loading...</p>
+  }
+  
   return (
     <div>
-      
+      {movies?.map((movie) => {
+        return (
+          <div key={movie._id}>
+            <Link 
+            to={`/intheaters/${movie._id}`}>
+              <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movies.original_title} />
+              <h3>{movie.original_title}</h3>
+            </Link>
+          </div>
+        )
+      })}
+      {loading && 'Loading movies'}
+      {error && error}
     </div>
   );
 }
