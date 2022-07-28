a2634dd51bf592bc862c105ad53ccc6c43d8b683
brianchoi93 <brianchoi93@gmail.com>
2022-07-28 09:44:49 -0700

    Make comingsoon component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom'
 import Main from './components/main/Main';
+import InTheaters from './components/inTheaters/InTheaters';
+import ComingSoon from './components/comingSoon/ComingSoon';
+import Movie from './components/Movie/Movie';
 
 function App() {
   return (
     <div>
       <header>
         <h1>Filmster</h1>
-        <ul>
-          <li> <Link to ='/'> Home </Link></li>
-          <li>In Theaters</li>
-          <li>Coming Soon</li>
-          {/* <li><Link to ='/'>Home</Link></li> */}
-        </ul>
+        <div>
+          <Link to ='/'> Home </Link>
+          <Link to ='/intheaters'> In Theaters </Link>
+          <Link to ='/comingsoon'> Coming Soon </Link>
+        </div>
       </header>
       <main>
         <Routes>
           <Route path='/' element={<Main/>} />
+          <Route path='/intheaters' element={<InTheaters/>}/>
+          <Route path='/intheaters/:id' element={<Movie/>}/>
+          <Route path='/comingsoon' elemnent={<ComingSoon/>}/>
         </Routes>
       </main>
     </div>
--- src/components/comingSoon/ComingSoon.js
+++ src/components/comingSoon/ComingSoon.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ComingSoon(props) {
   return (
     <div>
-      
+      What movies are coming out in the near future?
     </div>
   );
 }
