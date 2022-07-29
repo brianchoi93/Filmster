6e89bfd4024619134b76132c60522a33fa006a7b
brianchoi93 <brianchoi93@gmail.com>
2022-07-28 22:19:00 -0700

    Make adjustments to components name

--- src/components/InTheatersMovie/InTheatersMovie.js
+++ src/components/InTheatersMovie/InTheatersMovie.js
@@ -1,52 +1,9 @@
-// import React from 'react';
-// import { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// // import { useContext } from 'react';
-// // import { DataContext } from '../../dataContext';
-
-// function Movie() {
-//   const [movie, setMovie] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState('');
-//   const { id } = useParams();
-
-//   useEffect(() => {
-//     setLoading(true);
-//     fetch(`http://localhost:8000/movies/${id}`)
-//       .then((res) => res.json())
-//       .then((json)=>{
-//         setMovie(json);
-//         setLoading(false);
-//       })
-//       .catch((err) => {
-//         console.log("error")
-//         setError('Something went wrong! Please try again.');
-//       })
-//   }, [id]);
-
-//   if (loading) {
-//     return <p>Data is loading...</p>
-//   }
-  
-//   return (
-//     <div>
-//       <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.original_title} />
-//       <h2>{movie.original_title}</h2>
-//       <h3>{movie.overview}</h3>
-//       <h5>Release Date: {movie.release_date}</h5>
-
-//       {loading && 'Loading Movie!'}
-//       {error && error}
-
-//     </div>
-//   );
-// }
 import React from 'react';
 import ReactPlayer from 'react-player';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-function Movie() {
+function InTheatersMovie() {
   const [ inTheaterMovie, setInTheaterMovie ] = useState([]);
   const [ inTheaterTrailer, setInTheaterTrailer ] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -97,4 +54,4 @@ function Movie() {
   );
 }
 
-export default Movie;
+export default InTheatersMovie;
